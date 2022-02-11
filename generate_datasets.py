3d4724459c77343d7b74ec2edab680dadc238295
Philipp Teutsch <philipp.teutsch@tu-ilmenau.de>
2022-02-11 15:46:54 +0100

    removed way to generate data with custom number of samples

--- generate_datasets.py
+++ generate_datasets.py
@@ -198,27 +198,27 @@ if __name__ == '__main__':
     args = parser.parse_args()
     force = args.force
 
-    systems: Dict[str, Tuple[float, float, float, Callable]] = {'roessler': (0.12, 0.069, None, do_roessler),
-                                          'lorenz': (0.01, 0.905, 12115300, do_lorenz),
-                                          'lorenz96': (0.05, 1.67, 8098800, do_lorenz96),
-                                          'thomas': (0.1, 0.055, 16589900, do_thomas),
-                                          'mackeyglass': (1.0, 0.006, 15833232, do_mackeyglass),
-                                          'hyperroessler': (0.1, 0.14, 11010420, do_hyperroessler)}
+    systems: Dict[str, Tuple[float, float, float, Callable]] = {'roessler': (0.12, 0.069, do_roessler),
+                                          'lorenz': (0.01, 0.905, do_lorenz),
+                                          'lorenz96': (0.05, 1.67, do_lorenz96),
+                                          'thomas': (0.1, 0.055, do_thomas),
+                                          'mackeyglass': (1.0, 0.006, do_mackeyglass),
+                                          'hyperroessler': (0.1, 0.14, do_hyperroessler)}
 
     for system, (dt, lle, sample_limit, fn) in systems.items():
         input_steps = 150.0
         samples = 10000
-        output_steps = 1.0 / lle / dt
+        theo_output_steps = 1.0 / lle / dt
+        output_steps = int(np.ceil(theo_output_steps))
         safety_factor = 5.0
         if sample_limit:
             max_limit = sample_limit * dt
         else:
-            max_limit = int(samples * (input_steps + output_steps) * safety_factor * dt)
-
+            max_limit = int(samples * (input_steps + theo_output_steps) * safety_factor * dt)
         print(f'Generating data from {system} with an LLE of {lle} and a dt of {dt}:')
         print(f'> ... {int((input_steps + output_steps) * samples * safety_factor)} states')
         print(f'> ... {samples} samples with a safety_factor of {safety_factor}')
         print(f'> ... {input_steps} input_steps')
-        print(f'> ... {output_steps:0d} output_steps')
+        print(f'> ... {output_steps} output_steps')
         _ = fn(dt=dt, lle=lle, limit=max_limit)
         print(50 * '-')
